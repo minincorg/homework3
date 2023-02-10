1845ef2fc5eca9855c8098d8a14aea1860a485be
minincorg <minincorg@yandex.ru>
2023-02-10 15:24:48 +0300

    Homework 3

---
+++ HomeworkTests_3/src/test/java/homework3/Test2.java
@@ -0,0 +1,47 @@
+package homework3;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.util.concurrent.TimeUnit;
+
+public class Test2 {
+    public static void main(String[] args) {
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--incognito");
+        options.addArguments("start-maximized");
+
+        WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+        driver.get("https://www.livejournal.com");
+
+
+        WebElement webElement1 = driver.findElement(By.cssSelector(".s-header-item__link--login"));
+        WebElement webElement2 = driver.findElement(By.id("user"));
+        WebElement webElement3 = driver.findElement(By.id("lj_loginwidget_password"));
+        WebElement webElement4 = driver.findElement(By.name("action:login"));
+
+
+        webElement1.click();
+        webElement2.click();
+        webElement2.sendKeys("minincorg");
+        webElement3.click();
+        webElement3.sendKeys("Aa12042003");
+        webElement4.click();
+
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        driver.navigate().to("https://text-ru.livejournal.com/2768.html?media&ila_campaign=medius&ila_location=main&ila_context=main_block");
+
+        WebElement webElement5 = driver.findElement(By.className("j-user-subscribe-btn"));
+        webElement5.click();
+    }
+}
